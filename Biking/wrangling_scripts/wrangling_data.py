961801ff18d6017a5e7d7161353662496be002ce
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2021-01-04 21:32:09 -0800

    save before deployment

--- Biking/wrangling_scripts/wrangling_data.py
+++ Biking/wrangling_scripts/wrangling_data.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
@@ -8,7 +7,6 @@ import seaborn as sns
 from sklearn.utils import shuffle
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-import shap
 import plotly.graph_objs as go
 import plotly.express as px
 from sklearn.decomposition import PCA
